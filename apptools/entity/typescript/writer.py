0f0558d0ea8ff55ee39ce221b6b02dbb89cf4ceb
Erik Versteeg <erik@dexels.com>
2020-01-13 08:27:24 +0100

    Set correct file separator

--- apptools/entity/typescript/writer.py
+++ apptools/entity/typescript/writer.py
@@ -50,7 +50,7 @@ def _rel_import(source: pathlib.Path, destination: pathlib.Path,
     # current dir dot.
     # For example: "./Official" becomes "Official" which is not the same as an
     # import statement.
-    return str(os.path.relpath(destination, source)) + "/" + name
+    return str(pathlib.Path(os.path.relpath(destination, source)).as_posix()) + "/" + name
 
 
 def _dependencies(message: Message) -> List[Entity]:

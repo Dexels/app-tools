19e41e844bcd2de870198b02cfd09d63b18e11ad
StefanICT <stefan@dexels.com>
2020-03-16 07:56:30 +0100

    Variable names should be in snakecase

--- apptools/entity/typescript/writer.py
+++ apptools/entity/typescript/writer.py
@@ -54,11 +54,11 @@ def _rel_import(source: pathlib.Path, destination: pathlib.Path,
     # current dir dot.
     # For example: "./Official" becomes "Official" which is not the same as an
     # import statement.
-    tmpPath = pathlib.Path(os.path.relpath(destination, source)).as_posix()
+    tmp_path = pathlib.Path(os.path.relpath(destination, source)).as_posix()
     prefix = ""
-    if not tmpPath.startswith("."):
+    if not tmp_path.startswith("."):
         prefix = "./"
-    return prefix + str(tmpPath) + "/" + name
+    return prefix + str(tmp_path) + "/" + name
 
 
 def _dependencies(message: Message) -> List[Entity]:
